8286439a3418397c8a07bd91523b45d068956332
Kshitij Gupta <kshitij.gupta9137@gmail.com>
2026-03-25 12:55:22 +0530

    Phase 3A: Document, ExtractedText, DocumentChunk models created

--- askdocs_backend/settings.py
+++ askdocs_backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,3 +158,6 @@ SIMPLE_JWT = {
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Document, ExtractedText, DocumentChunk
 
-# Register your models here.
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'file_size', 'upload_date', 'is_processed')
+    list_filter = ('is_processed', 'upload_date')
+    search_fields = ('title', 'owner__username')
+
+
+@admin.register(ExtractedText)
+class ExtractedTextAdmin(admin.ModelAdmin):
+    list_display = ('document', 'page_number')
+    search_fields = ('document__title',)
+
+
+@admin.register(DocumentChunk)
+class DocumentChunkAdmin(admin.ModelAdmin):
+    list_display = ('document', 'chunk_index', 'page_number')
+    search_fields = ('document__title',)
